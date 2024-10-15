27268198298e5c3302d720104a69e02d7eabea27
Chad Jemmett <chad.jemmett@gmail.com>
2024-10-15 15:00:17 -0500

    groundwork for authentication and account careattion"

--- omaf_app/views.py
+++ omaf_app/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+    return HttpResponse("This is the omaf app index view")
+
+
+def register(request):
+    return HttpResponse("Registration page")
+
+def login(request):
+    return HttpResponse("Login page")
+
+
+def dashboard(request):
+    return HttpResponse("Dashboard page")
